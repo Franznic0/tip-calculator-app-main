304cf54f387515c428f0b2c892ab89d9bce19a9e
Franznic0 <franznicofranz@gmail.com>
2024-04-03 18:36:08 +0100

    Total and people check

--- main.js
+++ main.js
@@ -1,96 +1,74 @@
-var bill = document.getElementById("bill-amount");
-var tip = document.querySelectorAll("tip");
-var btn = document.querySelectorAll("btn");
-console.log(tip);
-var people = document.getElementById("num-people");
-var custom = document.querySelector("custom-input");
+// input
+var bill = document.querySelector("#bill-amount");
+const tipSelected = document.querySelectorAll("tip");
+var custom = document.querySelector("#custom-input");
+var people = document.querySelector("#num-people");
+// output
+var tipAmount = document.querySelector('.tip-amount');
+var totAmount = document.querySelector('.tot-amount');
+// error people
 var errorMessage = document.getElementById("error-message");
+var reset = document.querySelector('#reset');
 
+// get values
+document.oninput = () => {
+    calcTip();
+    calcTot();
+    totTip.innerHTML = totTip;
+    totAmount.innerHTML = total;
 
-//set bill value
-
-let billValue = 0;
-
-function validateFloat(x) {
-    var rgx = /^[0-9]*\.?[0-9]*$/;
-    return x.match(rgx);
 }
-
-bill.addEventListener('input', setBillValue);
-
-function setBillValue() {
-
-    if (bill.value.includes(",")) {
-        bill.value = bill.value.replace(',', '.');
-    }
-
-    if (!validateFloat(bill.value)) {
-        bill.value = bill.value.substring(0, bill.value.lenght-1);
-    }
-
-    billValue = parseFloat(bill.value);
-};
-
-
-//manage tip
-
-let tipValue = 0;
-let customValue = 0;
-
-function handleClick(event) {
-    tip.forEach(btn => {
-
-        btn.classList.remove("tip-active");
-
-        if (event.target.value == btn.value) {
-
-            btn.classList.add("tip-active");
-            tipValue = parseFloat(tip.value)/100;
-        }
-    })
-}
-
-
-
-//set people value
-
-let peopleValue = 0;
-
-function validatePeople(z) {
-        var rgx = /^[0-9]*/;
-        return z.match(rgx);
-;}
-
-
-people.addEventListener('input', setPeopleValue);
-function setPeopleValue() {
-
-    if (!validatePeople(people.value)) {
-        people.value = people.value.substring(0, people.value.lenght-1);
-    }
-
-    peopleValue = parseFloat(people.value);
-
-    //call error message on people 0
-    error();
-};
-
-
-//display error message on people 0
-function error() {
-    if (peopleValue == 0) {
-        errorMessage.style.display = "block";
-        people.style.border = "1px solid rgb(255, 0, 0)";
+// check n people
+people.oninput = () => {
+    if (people.value == 0) {
+        people.classList.add('active');
+        errorMessage.classList.add('active');
     } else {
-        errorMessage.style.display = "none";
-        people.style.border = "";
-    };
-};
-
-// calculation function
-
-function calculate() {
-    
-        let totalValue = (billValue * tipValue / peopleValue);
-        
+        people.classList.remove('active');
+        errorMessage.classList.remove('active');
+    }
 }
+// calculate tip
+let tip = 0;
+function tipSelection (){
+    tipSelected.forEach((tipSelection)=>{
+        tipSelection.addEventListener('click', ()=>{
+            tip = tipSelection.value;
+        })
+    })
+
+}
+
+let totTip = 0;
+function calcTip() {
+    tipSelection();
+
+    if (tip !== 0) {
+        totTip = "$" + Math.round((bill.value * tip / 100)*100)/100;
+    } else {
+        totTip = "$0.00";
+    }
+}
+
+// calculate total
+let total = 0
+function calcTot() {
+    
+    if (bill.value>0 && people.value>0){
+        total = "$" + Math.round((bill.value / people.value)*100)/100;
+    } else {
+        return total = "$0.00";
+    }
+}
+
+// reset
+reset.addEventListener('click', ()=> {
+    if (people.classList.contains('active')){
+        people.classList.remove('active');
+        errorMessage.classList.remove('active');
+        totTip = "$0.00";
+        totAmount = "$0.00";
+    } else {
+        return;
+    }
+})
